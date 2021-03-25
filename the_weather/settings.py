9fc508447e1dda0f4ae8bb3b9060e99a2faec02c
Shivam yadav <shivamyadav.cse171168@gmail.com>
2021-03-25 21:19:43 +0530

    Update settings.py
    revoked secret key

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q2(@$@d^dahgg-46+tpcwhp%@%e8l8u4j!!5gcvw8b7hc3+mt0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
